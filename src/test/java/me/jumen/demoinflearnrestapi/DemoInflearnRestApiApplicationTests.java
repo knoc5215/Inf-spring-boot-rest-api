3de9f32cb084a2a5cd59a49f9b95dc2d6e4c989c
knoc5215 <knoc5215@naver.com>
2020-12-30 21:01:14 +0900

    Initial commit

---
+++ src/test/java/me/jumen/demoinflearnrestapi/DemoInflearnRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jumen.demoinflearnrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoInflearnRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
